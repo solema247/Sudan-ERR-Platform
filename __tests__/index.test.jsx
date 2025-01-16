c9f4179c3cfdae10cf425f7680ec2ec57962095d
John Gorenfeld <john@gorenfeld.net>
2025-01-16 13:25:24 +0000

    First Jest test

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index'
+
+describe('Index', () => {
+    it('loads calculator decoy', () => {
+        render(<Home/>)
+
+        const buttons = screen.getAllByRole('button')
+        expect(buttons.length).toBeGreaterThan(0)
+    })
+})
