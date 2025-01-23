ae00298bb8d04d811174a10e21fd6e9af18a8f82
John Gorenfeld <john@gorenfeld.net>
2025-01-23 15:08:46 +0000

    broke off separate ExpenseCard

---
+++ components/forms/Report/ExpenseCard.tsx
@@ -0,0 +1,70 @@
+import { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
+import ReceiptChooser from './ReceiptUploader';
+import { useTranslation } from 'react-i18next';
+
+
+const ExpenseCard = ({ expense, index, arrayHelpers }) => {
+    const t = useTranslation('fill-form');
+    
+    return (
+        <div key={index} >
+            <Field
+                name={`expenses[${index}].activity`}
+                type="text"
+                placeholder="Activity"
+            />
+            <Field
+                name={`expenses[${index}].description`}
+                type="text"
+                placeholder="Description"
+            />
+            <Field
+                name={`expenses[${index}].payment_date`}
+                type="date"
+            />
+            <Field
+                name={`expenses[${index}].seller`}
+                type="text"
+                placeholder="Seller"
+            />
+            <Field
+                name={`expenses[${index}].payment_method`}
+                as="select"
+            >
+                <option value="cash">Cash</option>
+                <option value="credit">Credit</option>
+                <option value="debit">Debit</option>
+            </Field>
+            <Field
+                name={`expenses[${index}].receipt_no`}
+                type="text"
+                placeholder="Receipt No."
+            />
+            <Field
+                name={`expenses[${index}].amount`}
+                type="number"
+                placeholder="Amount"
+            />
+            <Field
+                name={`expenses[${index}].receiptFile`}
+                type="file"
+            />
+            <ReceiptChooser
+                onFileSelect={(file) => arrayHelpers.setFieldValue(`expenses.${index}.file`, file)}
+                onError={(error) => alert(error)}
+            />
+
+            <button
+                type="button"
+                className="text-red-500 mt-2 font-bold"
+                onClick={() => arrayHelpers.remove(index)}
+            >
+                {t('remove')}
+            </button>
+                </div>
+            )
+
+           
+}
+
+export default ExpenseCard
--- components/forms/Report/ReportingForm.tsx
+++ components/forms/Report/ReportingForm.tsx
@@ -3,8 +3,8 @@ import { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
 import { useTranslation } from 'react-i18next';
 import FormBubble from '../../ui/FormBubble';
 import Button from '../../ui/Button';
-import ReceiptChooser from './ReceiptUploader';
 import { supabase } from '../../../services/supabaseClient';
+import ExpenseCard from './ExpenseCard';
 import getInitialValues from './values';
 import getValidationSchema from './validation';
 import onSubmit from './onSubmit';
@@ -14,23 +14,11 @@ const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB in bytes.
 
 /**
  * F4 Financial Reporting form, called from Menu.tsx
- * 
-    language
  */
-
-// TODO: Ensure we are uploading receipts
-// TODO: Differentiate somehow between form and supporting files.
-// TODO: Prepopulate ERR ID, first expense.
-// TODO: Report ID should be assigned when upload begins, right? Or at creation?
-// TODO: Wire uploader back up.
-// TODO: Wire back up errors on individual array items. How does this
-// TODO: Wire back up the onBlur, etc. handlers if we need them.
-
 
 const ReportingForm = ({ errId, project, onReturnToMenu, onSubmitAnotherForm }) => {
     const { t } = useTranslation('fillForm');
     const [categories, setCategories] = useState([]);
-    // const reportId = getReportId();
 
     useEffect(() => {
         const populateCategories = async () => {
@@ -48,108 +36,114 @@ const ReportingForm = ({ errId, project, onReturnToMenu, onSubmitAnotherForm }) 
 
     const initialValues = getInitialValues(errId);
     const validationSchema = getValidationSchema();
-   
+
     return (
         <FormBubble>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+            >
+                {({ isSubmitting, values }) => (
+                    <Form className="flex flex-col">
+                        <p className="text-3xl">{t('formTitle')}</p>
 
-        <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}
+                        <label htmlFor="err_id" className="font-bold block text-base text-black-bold mb-1">
+                            {t('errId')}
+                        </label>
+                        <Field type="text" name="err_id" />
+                        <ErrorMessage name="err_id" component="div" />
 
-        >
-        {({ isSubmitting, values }) => (
-         <Form className="flex flex-col">
-            <p className="text-3xl">{t('formTitle')}</p>
+                        <label htmlFor="date" className="font-bold block text-base text-black-bold mb-1">
+                            {t('date')}
+                        </label>
+                        <Field type="date" name="date" />
+                        <ErrorMessage name="date" component="div" />
 
-            <label htmlFor="err_id" className="font-bold block text-base text-black-bold mb-1">{t('errId')}</label>
-            <Field type="text" name="err_id"/>
-            <ErrorMessage name="err_id" component="div" />
-            
-            <label htmlFor="date" className="font-bold block text-base text-black-bold mb-1">{t('date')}</label>
-            <Field type="date" name="date"/>
-            <ErrorMessage name="date" component="div" />
+                        <label htmlFor="total_grant" className="font-bold block text-base text-black-bold mb-1">
+                            {t('totalGrant')}
+                        </label>
+                        <Field type="number" name="total_grant" min="0" />
+                        <ErrorMessage name="total_grant" component="div" />
 
-            <label htmlFor="total_grant" className="font-bold block text-base text-black-bold mb-1">{t('totalGrant')}</label>
-            <Field type="number" name="total_grant" min="0"/>
-            <ErrorMessage name="date" component="div" />
+                        <label htmlFor="other_sources" className="font-bold block text-base text-black-bold mb-1">
+                            {t('totalOtherSources')}
+                        </label>
+                        <Field type="number" name="total_other_sources" min="0" />
+                        <ErrorMessage name="total_other_sources" component="div" />
 
-            <label htmlFor="other_sources" className="font-bold block text-base text-black-bold mb-1">{t('totalOtherSources')}</label>
-            <Field type="number" name="total_other_sources" min="0"/>
-            <ErrorMessage name="total_other_sources" component="div" />
+                        <h3 className="text-2xl font-bold pt-4">Activities and Expenses</h3>
 
-            <h3 className="text-2xl font-bold pt-4">Activities and Expenses</h3>
-
-             <FieldArray
-                name="expenses"
-                render={(arrayHelpers) => (
-                        <div>
-                            {values.expenses.map((expense, index) => (
-                            <div>
-                                    {/* <ReceiptChooser
-                                        onFileSelect={(file) => setFieldValue(`expenses.${index}.file`, file)}
-                                        onError={(error) => alert(error)}
-                                    /> */}
-
-                                <button
-                                    type="button"
-                                    className="text-red-500 mt-2 font-bold"
-                                    onClick={() => arrayHelpers.remove(index)}
-                                >
-                                {t('remove')}
-                                </button>
+                        <FieldArray
+                            name="expenses"
+                            render={(arrayHelpers) => (
+                                <div>
+                                    {values.expenses.map((expense, index) => (
+                                        <ExpenseCard
+                                            key={index}
+                                            expense={expense}
+                                            index={index}
+                                            arrayHelpers={arrayHelpers}
+                                        />
+                                    ))}
+                                    <Button
+                                        text={t('addExpense')}
+                                        onClick={() =>
+                                            arrayHelpers.push({
+                                                activity: '',
+                                                description: '',
+                                                payment_date: '',
+                                                seller: '',
+                                                payment_method: 'cash',
+                                                receipt_no: '',
+                                                amount: '',
+                                                file: null,
+                                            })
+                                        }
+                                        className="bg-blue-500 text-white mt-4"
+                                    />
                                 </div>
-                            ))}
+                            )}
+                        />
 
-                            <Button
-                                text={t('addExpense')}
-                                onClick={() =>
-                                    arrayHelpers.push({
-                                        activity: '',
-                                        description: '',
-                                        payment_date: '',
-                                        seller: '',
-                                        payment_method: 'cash',
-                                        receipt_no: '',
-                                        amount: '',
-                                        file: null,
-                                    })
-                                }
-                                className="bg-blue-500 text-white mt-4"
-                            />
-                        </div>
+                        <label htmlFor="excess_expenses" className="font-bold block text-base text-black-bold mb-1">
+                            {t('excessExpenses')}
+                        </label>
+                        <Field type="text" name="excessExpenses" />
+                        <ErrorMessage name="excessExpenses" component="div" />
+
+                        <label htmlFor="surplus_use" className="font-bold block text-base text-black-bold mb-1">
+                            {t('surplusUse')}
+                        </label>
+                        <Field type="text" name="surplus_use" />
+                        <ErrorMessage name="surplus_use" component="div" />
+
+                        <label htmlFor="training" className="font-bold block text-base text-black-bold mb-1">
+                            {t('training')}
+                        </label>
+                        <Field type="text" name="training" />
+                        <ErrorMessage name="training" component="div" />
+
+                        <label htmlFor="lessons" className="font-bold block text-base text-black-bold mb-1">
+                            {t('lessons')}
+                        </label>
+                        <Field type="text" name="lessons" />
+                        <ErrorMessage name="lessons" component="div" />
+
+                        <label htmlFor="total_expenses" className="font-bold block text-base text-black-bold mb-1">
+                            {t('totalExpenses')}
+                        </label>
+                        <Field type="text" name="total_expenses" />
+                        <ErrorMessage name="total_expenses" component="div" />
+
+                        <button type="submit" disabled={isSubmitting}>
+                            Submit
+                        </button>
+                    </Form>
                 )}
-            />
-
-            <label htmlFor="excess_expenses" className="font-bold block text-base text-black-bold mb-1">{t('excessExpenses')}</label>
-            <Field type="text" name="excessExpenses"/>
-            <ErrorMessage name="excessExpenses" component="div" />
-
-            <label htmlFor="surplus_use" className="font-bold block text-base text-black-bold mb-1">{t('surplusUse')}</label>
-            <Field type="text" name="surplus_use"/>
-            <ErrorMessage name="surplus_use" component="div" />
-
-            <label htmlFor="training" className="font-bold block text-base text-black-bold mb-1">{t('training')}</label>
-            <Field type="text" name="training"/>
-            <ErrorMessage name="training" component="div" />
-
-            <label htmlFor="lessons" className="font-bold block text-base text-black-bold mb-1">{t('lessons')}</label>
-            <Field type="text" name="lessons"/>
-            <ErrorMessage name="lessons" component="div" />
-
-            <label htmlFor="total_expenses" className="font-bold block text-base text-black-bold mb-1">{t('totalExpenses')}</label>
-            <Field type="text" name="total_expenses"/>
-            <ErrorMessage name="totalExpenses" component="div" />
-
-           <button type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-         </Form>
-       )}
-        </Formik>
+            </Formik>
         </FormBubble>
     );
-}
-       
+};
 
 export default ReportingForm;
