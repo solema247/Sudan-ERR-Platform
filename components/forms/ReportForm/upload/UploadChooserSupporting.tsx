47453b0ad916cb72c86de268e854299262ae5613
John Gorenfeld <john@gorenfeld.net>
2025-02-19 16:38:51 +0000

    ...

--- components/forms/ReportForm/upload/UploadChooserSupporting.tsx
+++ components/forms/ReportForm/upload/UploadChooserSupporting.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 import { UploadedList } from "./UploadedList";
 import { FileWithProgress } from "./UploadInterfaces";
 import { supabase } from "../../../../services/supabaseClient";
-import * as tus from "tus-js-client";
+import performUpload from './performUpload';
 
 export enum reportUploadType {
   RECEIPT,
@@ -30,7 +30,9 @@ export const UploadChooserSupporting: React.FC<UploadChooserProps> = ({
 
   const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
 
-    const selectedFiles: FileWithProgress[] = Array.from(e.target.files || []).map((file) => ({
+  const BUCKET_NAME = "images";
+
+  const selectedFiles: FileWithProgress[] = Array.from(e.target.files || []).map((file) => ({
       file,
       uploaded: false,
       progress: 0,
@@ -38,14 +40,13 @@ export const UploadChooserSupporting: React.FC<UploadChooserProps> = ({
     }));
 
     setFiles((prevState) => [...prevState, ...selectedFiles]);
-    selectedFiles.forEach((fileObj, index) => beginFileUpload(fileObj, index));     
+    selectedFiles.forEach((file, index) => performUpload(BUCKET_NAME, "filenameTODO", file)); 
   };
 
   const removeFile = (index: number) => {
     setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
   };
 
-  const supabaseProjectId = "inrddslmakqrezinnejh";
 
   return (
     <div className="max-w-lg mx-auto">
@@ -91,51 +92,4 @@ const UploadBox: React.FC<UploadBoxProps> = ({ onFileChange, uploadType }) => {
     </div>
   );
 };
-
-async function uploadFile(bucketName, fileName, file) {
-    const { data: { session } } = await supabase.auth.getSession()
-
-    return new Promise((resolve, reject) => {
-        var upload = new tus.Upload(file, {
-            endpoint: `https://${projectId}.supabase.co/storage/v1/upload/resumable`,
-            retryDelays: [0, 3000, 5000, 10000, 20000],
-            headers: {
-                authorization: `Bearer ${session.access_token}`,
-                'x-upsert': 'true', // optionally set upsert to true to overwrite existing files
-            },
-            uploadDataDuringCreation: true,
-            removeFingerprintOnSuccess: true, // Important if you want to allow re-uploading the same file https://github.com/tus/tus-js-client/blob/main/docs/api.md#removefingerprintonsuccess
-            metadata: {
-                bucketName: bucketName,
-                objectName: fileName,
-                contentType: 'image/png',
-                cacheControl: '3600'
-            },
-            chunkSize: 6 * 1024 * 1024, // NOTE: it must be set to 6MB (for now) do not change it
-            onError: function (error) {
-                console.log('Failed because: ' + error)
-                reject(error)
-            },
-            onProgress: function (bytesUploaded, bytesTotal) {
-                var percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2)
-                console.log(bytesUploaded, bytesTotal, percentage + '%')
-            },
-            onSuccess: function () {
-                console.log("Uploaded: " + upload.url)
-                resolve(true)   // TODO: Check that this is the type we want.
-            },
-        })
-
-        return upload.findPreviousUploads().then(function (previousUploads) {
-            // Found previous uploads so we select the first one.
-            if (previousUploads.length) {
-                upload.resumeFromPreviousUpload(previousUploads[0])
-            }
-
-            // Start the upload
-            upload.start()
-        })
-    })
-}
-
 
